67299e4a179f9b0b10b9e4134b2347996506a81a
QuangBinh <ngqbinh456@gmail.com>
2025-08-12 06:56:55 +0700

    Complete CICD configuration final

---
+++ lighthouserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000'],
+      numberOfRuns: 3
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', {minScore: 0.8}],
+        'categories:accessibility': ['error', {minScore: 0.9}],
+        'categories:best-practices': ['warn', {minScore: 0.8}],
+        'categories:seo': ['warn', {minScore: 0.8}]
+      }
+    }
+  }
+}
